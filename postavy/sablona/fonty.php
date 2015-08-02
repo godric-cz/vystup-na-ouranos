cde071ab2a5921706110ee08600476ccb37afb71
Godric <godric@korh.cz>
2015-08-02 16:02:39 +0200

    provizorní řešení open fontů

--- postavy/sablona/fonty.php
+++ postavy/sablona/fonty.php
@@ -1,17 +1,23 @@
+<?php
+
+if(is_file(__DIR__.'/calibri.ttf')) $font = '/calibri';
+else $font = '/carlito';
+
+?>
 
 @font-face {
   font-family: 'SomeFont';
-  src: url(data:font/truetype;charset=utf-8;base64,<?=base64_encode(file_get_contents(__DIR__.'/calibri.ttf'))?>);
+  src: url(data:font/truetype;charset=utf-8;base64,<?=base64_encode(file_get_contents(__DIR__.$font.'.ttf'))?>);
 }
 
 @font-face {
   font-family: 'SomeFont';
   font-weight: bold;
-  src: url(data:font/truetype;charset=utf-8;base64,<?=base64_encode(file_get_contents(__DIR__.'/calibrib.ttf'))?>);
+  src: url(data:font/truetype;charset=utf-8;base64,<?=base64_encode(file_get_contents(__DIR__.$font.'b.ttf'))?>);
 }
 
 @font-face {
   font-family: 'SomeFont';
   font-style: italic;
-  src: url(data:font/truetype;charset=utf-8;base64,<?=base64_encode(file_get_contents(__DIR__.'/calibrii.ttf'))?>);
+  src: url(data:font/truetype;charset=utf-8;base64,<?=base64_encode(file_get_contents(__DIR__.$font.'i.ttf'))?>);
 }
